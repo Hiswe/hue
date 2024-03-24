b2b8646afeee8a29a9fc6f52014fafd8af7d9c3c
Hiswe <hiswehalya@gmail.com>
2024-03-24 20:25:36 +0100

    feat: spinner

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HBtn: typeof import('./src/components/HBtn.vue')['default']
+    HSpinner: typeof import('./src/components/HSpinner.vue')['default']
     HSwitch: typeof import('./src/components/HSwitch.vue')['default']
     HTextInput: typeof import('./src/components/HTextInput.vue')['default']
     IconoirTrash: typeof import('~icons/iconoir/trash')['default']

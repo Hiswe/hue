a89ac3e5840b9cb07fd40336e3f05b6875f9cf54
Hiswe <hiswehalya@gmail.com>
2024-07-14 09:06:33 +0200

    chore(dependencies): update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router/auto';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from 'vue-router/auto-routes';
 
 import App from './App.vue';
 import './app.css';
 
 const app = createApp(App);
-const router = createRouter({
-  history: createWebHistory(),
-});
+const history = createWebHistory();
+const router = createRouter({ history, routes });
 
 app.use(router);
 
